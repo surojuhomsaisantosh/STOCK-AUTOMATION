3417a91b4cbff0fb60b794df1df389922bfb1efd
suroju-hom-sai-santosh <surojuhomsaisantosh@gmail.com>
2026-01-24 20:35:04 +0530

    Final config for re-deployment

--- stock-automation/vite.config.js
+++ stock-automation/vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // <--- ADD THIS LINE
+  base: '/', 
+  build: {
+    outDir: 'dist' // Explicitly tell Vite to output to 'dist'
+  }
 })
